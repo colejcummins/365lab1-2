589d2ce3d6f597a22c12a2be2d6bcf2fc3a4fa1a
Cole Cummins <cole_cummins@carbon3d.com>
2019-09-30 13:03:06 -0700

    finish

--- schoolsearch.py
+++ schoolsearch.py
@@ -36,29 +36,44 @@ def prompt_loop(students):
     '[B[us]]\nT[eacher]: <lastname>\nB[us]: <number>\nG[rade]: <number>'
     '[H[igh]|L[ow]]\nA[verage]: <number>\nI[nfo]\nQ[uit]\n\n')
 
+    grades = students_by_grade(students)
+
     while True:
         inp = input(prompt)
         split = inp.split()
 
-        if check(split[0], "Info"):
-            grades = [0 for _ in range(6)]
-            for s in students:
-                grades[int(s.Grade) - 1] += 1
+        if check(split, "Info"):
             print("\n".join(str(i + 1) + ":" + str(grades[i]) for i in range(len(grades))))
 
-        if check(split[0], "Quit"):
+        elif check(split, "Quit"):
             break
 
-        if len(split) < 2:
-            continue
+        elif check(split, "Average:", ":", 2):
+            sum_gpa = 0.0
+            for s in students:
+                sum_gpa += float(s.GPA) if s.Grade == split[1] else 0
+            if grades[int(split[1]) - 1] > 0:
+                print(split[1], round(sum_gpa/grades[int(split[1]) - 1], 2))
 
-        if check(split[0], "Teacher:", ":"):
+        elif check(split, "Grade:", ":", 2):
+            attr = ["StLastName", "StFirstName", "GPA", "Bus", "TLastName", "TFirstName"]
+            if len(split) == 2:
+                seq_search("Grade", split[1], attr, students)
+            elif split[2] == "High" or split[2] == "H":
+                high = Student(["", "", "", "", "", "0.0", "", ""])
+                find_max_min(students, grades, split, attr, high, lambda a, b: float(a) > float(b))
+
+            elif split[2] == "Low" or split[2] == "L":
+                low = Student(["", "", "", "", "", "100.0", "", ""])
+                find_max_min(students, grades, split, attr, low, lambda a, b: float(a) < float(b))
+
+        elif check(split, "Teacher:", ":", 2):
             seq_search("TLastName", split[1], ["StLastName", "StFirstName"], students)
 
-        if check(split[0], "Bus:", ":"):
+        elif check(split, "Bus:", ":", 2):
             seq_search("Bus", split[1], ["StLastName", "StFirstName", "Grade", "Classroom"], students)
 
-        if check(split[0], "Student:", ":"):
+        elif check(split, "Student:", ":", 2):
             if len(split) == 3 and (split[2] == "B" or split[2] == "Bus"):
                 seq_search("StLastName", split[1], ["StLastName", "StFirstName", "Bus"], students)
             else:
@@ -67,12 +82,21 @@ def prompt_loop(students):
 
         print("")
 
+def students_by_grade(students):
+    grades = [0 for _ in range(6)]
+    for s in students:
+        grades[int(s.Grade) - 1] += 1
+    return grades
 
-def check(inp, word, add=""):
-    return inp == word or inp == word[0] + add
+def find_max_min(students, grades, split, attr, start, comp):
+    for s in students:
+        if s.Grade == split[1] and comp(s.GPA, start.GPA):
+            start = s
+    if grades[int(split[1]) - 1] > 0:
+        print(",".join(getattr(start, a) for a in attr))
 
-if __name__ == "__main__":
-    main()
+def check(split, word, add="", exp_len=1):
+    return (split[0] == word or split[0] == word[0] + add) and len(split) >= exp_len
 
 # attr: string, inp: string, print_attrs: list
 # Takes the attribute to search, the given input, and a list of attributes to print, prints out all students within
@@ -83,3 +107,6 @@ def seq_search(attr, inp, print_attrs, students):
         if getattr(s, attr) == inp:
             str += ",".join(getattr(s, a) for a in print_attrs) + "\n"
     print(str)
+
+if __name__ == "__main__":
+    main()
